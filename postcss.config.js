e466c255e60d2bd1f59d07dc9a093469b486eacc
Brendan Loudermilk <bloudermilk@gmail.com>
2020-01-01 18:37:33 +0100

    Fix purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
   content: [
     './src/**/*.html',
-    './src/**/*.jsx',
+    './src/**/*.js',
   ],
 
   // Include any special characters you're using in this regular expression
