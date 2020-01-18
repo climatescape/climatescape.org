f6762e7f076010cbed4baf101a4281ba49516c1b
Brendan Loudermilk <bloudermilk@gmail.com>
2020-01-18 13:27:50 +0100

    Use email vs twitter for contact

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,14 +28,10 @@ const Layout = ({ children, contentClassName }) => {
     <div className="flex flex-col min-h-screen antialiased">
       <Navbar siteTitle={data.site.siteMetadata.title} />
       <main className={`flex-grow ${contentClassName}`}>{children}</main>
-      <footer className="text-gray-800 p-6 bg-gray-200">
-        <span>© Climatescape</span>
+      <footer className="text-gray-800 p-3 bg-gray-200 text-center sm:text-left">
+        <span>© Climatescape <a href="https://creativecommons.org/licenses/by-sa/4.0/" className="underline hover:no-underline">CC BY-SA</a></span>
         <span> • </span>
-        <a href="https://creativecommons.org/licenses/by-sa/4.0/" className="underline hover:no-underline">CC BY-SA 4.0</a>
-        <span> • </span>
-        <span>
-          Made&nbsp;by&nbsp;<a href="https://twitter.com/bloudermilk" className="underline hover:no-underline">@bloudermilk</a>
-        </span>
+        <a href="mainto:brendan@sinceresoftware.co" className="underline hover:no-underline">Contact</a>
       </footer>
     </div>
   )
