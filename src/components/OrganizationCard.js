4c8ec2ad67fbdeb6d09c171bf3323c4332bcbe6a
James Crowley <james.crowley@gmail.com>
2020-01-22 15:53:37 +0000

    Prep the Airtable data separately
    Separate the component wireup and internals from the mapping / parsing & filtering of Airtable data. We're about to start filtering on location for instance, and so need the filtering to take place on the same data we're displaying

--- src/components/OrganizationCard.js
+++ src/components/OrganizationCard.js
@@ -4,10 +4,6 @@ import Tag from "./Tag"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUsers, faLocationArrow, faBuilding, faTag } from '@fortawesome/free-solid-svg-icons'
 
-function filterDuplicateAndEmptyItems(...items) {
-  return [...new Set(items)].filter(m=>m);
-}
-
 const OrganizationCard = ({ title, description, tags, homepage, location, logo, headcount, orgType, onPickTag, activeTag }) => (
   <div className="flex border-b border-gray-400 p-3 text-gray-900">
     <div className="m-1 mr-5 w-24 flex-shrink-0">
@@ -32,8 +28,8 @@ const OrganizationCard = ({ title, description, tags, homepage, location, logo, 
         }
       </div>
       <div className="mt-1">
-        {location.country && <span className="inline-block mt-1 bg-gray-200 rounded-full px-2 py-1 text-xs font-semibold text-gray-700 mr-2">
-          <FontAwesomeIcon icon={faLocationArrow} className="mr-1" />{filterDuplicateAndEmptyItems(location.city, location.state, location.country).join(', ')}
+        {location && <span className="inline-block mt-1 bg-gray-200 rounded-full px-2 py-1 text-xs font-semibold text-gray-700 mr-2">
+          <FontAwesomeIcon icon={faLocationArrow} className="mr-1" />{location}
         </span>}
         {headcount && 
         <span className="inline-block mt-1 bg-gray-200 rounded-full px-2 py-1 text-xs font-semibold text-gray-700 mr-2">
--- src/templates/sector.js
+++ src/templates/sector.js
@@ -12,16 +12,31 @@ const SectorTemplate = ({ data }) => {
 
   const name = data.airtable.data.Name
 
+  function filterDuplicateAndEmptyItems(...items) {
+    return [...new Set(items)].filter(m=>m);
+  }
+
   // Avoid breaking if the sector has no orgs + map out nested data object
-  let organizations = (data.airtable.data.Organizations || []).map(o => o.data)
+  let organizations = (data.airtable.data.Organizations || [])
+    .map(o => o.data)
+    .map(({ Name, About, Tags, Homepage, City, State_Province, Country, Tagline, Logo, Headcount, Organization_Type }) => ({
+      title: Name,
+      description: Tagline || About,
+      tags: Tags,
+      location: filterDuplicateAndEmptyItems(City, State_Province, Country).join(', '),
+      headcount: Headcount,
+      orgType: Organization_Type,
+      homepage: Homepage,
+      logo: Logo && Logo.localFiles[0] && Logo.localFiles[0].childImageSharp && Logo.localFiles[0].childImageSharp.fluid,
+    }));
 
   // Filter all organizations by tag if one is selected
   if (tagFilter) organizations = organizations.filter(organization =>
-    organization.Tags && organization.Tags.indexOf(tagFilter) >= 0
+    organization.tags && organization.tags.indexOf(tagFilter) >= 0
   )
 
   // Sort by name (ascending)
-  organizations = organizations.sort((a, b) => stringCompare(a.Name, b.Name))
+  organizations = organizations.sort((a, b) => stringCompare(a.title, b.title))
 
   return <Layout contentClassName="bg-gray-200">
     <div className="max-w-4xl mx-auto pb-4">
@@ -41,20 +56,18 @@ const SectorTemplate = ({ data }) => {
 
       <div className="bg-white">
         {
-          organizations.map(({ Name, About, Tags, Homepage, City, State_Province, Country, Tagline, Logo, Headcount, Organization_Type }, index) =>
+          organizations.map(({ title, description, tags, homepage, location, logo, headcount, orgType }, index) =>
             <OrganizationCard
-              title={Name}
-              description={Tagline || About}
-              tags={Tags}
-              activeTag={tag}
-              location={{city: City, state: State_Province, country: Country}}
-              headcount={Headcount}
-              orgType={Organization_Type}
-              homepage={Homepage}
-              logo={Logo && Logo.localFiles[0] && Logo.localFiles[0].childImageSharp && Logo.localFiles[0].childImageSharp.fluid}
+              title={title}
+              description={description}
+              tags={tags}
               activeTag={tagFilter}
+              location={location}
+              headcount={headcount}
+              orgType={orgType}
+              homepage={homepage}
+              logo={logo}
               key={index}
-              onPickTag={tag => setTag(tag)}
               onPickTag={tag => setTagFilter(tag)}
             />
           )
