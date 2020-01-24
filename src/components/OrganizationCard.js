ddd25ebd20eee800b54b508a07c9cdccd30b6d09
Brendan Loudermilk <bloudermilk@gmail.com>
2020-01-24 14:13:51 +0100

    Style tweaks

--- src/components/OrganizationCard.js
+++ src/components/OrganizationCard.js
@@ -3,18 +3,21 @@ import Img from "gatsby-image"
 import { OrganizationTag, OrganizationLocation, OrganizationHeadcount, OrganizationOrgType } from "../components/OrganizationAttributes"
 
 const OrganizationCard = ({ title, description, tags, homepage, location, logo, headcount, orgType, currentFilter, onApplyFilter }) => (
-  <div className="flex border-b border-gray-400 p-3 text-gray-900">
-    <div className="m-1 mr-5 w-24 flex-shrink-0">
+  <div className="flex items-center border-b border-gray-400 p-3 text-gray-900">
+    <div className="mr-5 w-16 flex-shrink-0 hidden sm:block">
     {logo &&
-      <Img fluid={logo} className="h-24 w-24" />
+      <Img fluid={logo} className="h-16 w-16" />
     }
     </div>
-    <div className="">
-      <a className="font-bold text-lg hover:text-teal-500" href={homepage} target="_blank" rel="noopener noreferrer">
-        {title}
-      </a>
-      <p className="max-h-12 overflow-hidden">{description}</p>
-      <div className="mt-3">
+    <div>
+      <p>
+        <a className="font-bold hover:text-teal-500 mr-2" href={homepage} target="_blank" rel="noopener noreferrer">
+          {title}
+        </a>
+
+        {description}
+      </p>
+      <div className="mt-1">
         {
           tags && tags.map((tag, i) =>
             <OrganizationTag
@@ -24,23 +27,24 @@ const OrganizationCard = ({ title, description, tags, homepage, location, logo, 
               text={tag} />
           )
         }
-      </div>
-      <div className="mt-1">
-        {location && 
-          <OrganizationLocation 
-            onClick={e => onApplyFilter.byLocation(location)} 
+        {location &&
+          <OrganizationLocation
+            onClick={e => onApplyFilter.byLocation(location)}
+            key='location'
             active={location === currentFilter.byLocation}
             text={location} />}
-        {headcount && 
-          <OrganizationHeadcount 
-            onClick={e => onApplyFilter.byHeadcount(headcount)} 
+        {headcount &&
+          <OrganizationHeadcount
+            onClick={e => onApplyFilter.byHeadcount(headcount)}
+            key='headcount'
             active={headcount === currentFilter.byHeadcount}
             text={headcount}
             />
         }
-        {orgType && 
-          <OrganizationOrgType 
-            onClick={e => onApplyFilter.byOrgType(orgType)} 
+        {orgType &&
+          <OrganizationOrgType
+            onClick={e => onApplyFilter.byOrgType(orgType)}
+            key='orgtype'
             active={orgType === currentFilter.byOrgType}
             text={orgType}
           />
