964641c8b8d0ffada74035c8cfca72f2511bb6aa
Brendan Loudermilk <bloudermilk@gmail.com>
2020-01-27 21:55:12 +0100

    Reconfigure navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,27 @@
 import React, { useState } from "react"
-import { Link } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 import SiteLogo from "../images/site-logo.svg"
 
-const Navbar = ({ siteTitle }) => {
+const Navbar = () => {
   const [open, setOpen] = useState(false)
+
+  const data = useStaticQuery(graphql`
+    query NavbarQuery {
+      site {
+        siteMetadata {
+          title
+          newsletterUrl
+        }
+      }
+    }
+  `)
 
   return (
     <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
       <Link className="flex items-center flex-shrink-0 text-white mr-6" to="/">
         <img src={SiteLogo} alt="Logo: Earth Network" className="h-8 w-8 fill-current mr-3" />
-        <span className="font-semibold text-xl">{siteTitle}</span>
+        <span className="font-semibold text-xl">{data.site.siteMetadata.title}</span>
       </Link>
 
       <div className="block sm:hidden">
@@ -21,16 +32,24 @@ const Navbar = ({ siteTitle }) => {
 
       <div className={`${open ? "block" : "hidden"} w-full flex-grow sm:flex sm:items-center sm:w-auto`}>
         <div className="text-sm sm:text-right sm:flex-grow">
+          <Link to="/#about" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4">
+            About
+          </Link>
+
           <Link to="/#organizations" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4">
             Organizations
           </Link>
 
-          <Link to="/#about" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4">
-            About
+          <Link to="/contribute" className="block mt-4 sm:inline-block sm:mt-0 text-teal-200 hover:text-white mr-4">
+            Contribute
           </Link>
         </div>
         <div>
-          <Link to="/contribute/" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 sm:mt-0" activeClassName="border-transparent text-teal-500 bg-white">Contribute</Link>
+          <a
+            href={data.site.siteMetadata.newsletterUrl}
+            className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 sm:mt-0"
+            activeClassName="border-transparent text-teal-500 bg-white"
+          >Subscribe</a>
         </div>
       </div>
     </nav>
