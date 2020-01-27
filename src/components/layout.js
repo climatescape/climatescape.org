eb3a6dd5fb37eb8da27b9e3b9b2c3a275e280520
Brendan Loudermilk <bloudermilk@gmail.com>
2020-01-27 21:55:43 +0100

    Reconfigure footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,12 +26,12 @@ const Layout = ({ children, contentClassName }) => {
 
   return (
     <div className="flex flex-col min-h-screen antialiased">
-      <Navbar siteTitle={data.site.siteMetadata.title} />
+      <Navbar />
       <main className={`flex-grow ${contentClassName}`}>{children}</main>
       <footer className="text-gray-800 p-3 bg-gray-200 text-center sm:text-left">
-        <span>© Climatescape <a href="https://creativecommons.org/licenses/by-sa/4.0/" className="underline hover:no-underline">CC BY-SA</a></span>
+        <span>© {data.site.siteMetadata.title} <a href="https://creativecommons.org/licenses/by-sa/4.0/" className="underline hover:no-underline">CC BY-SA</a></span>
         <span> • </span>
-        <a href="mailto:brendan@sinceresoftware.co" className="underline hover:no-underline">Contact</a>
+        <a href="https://twitter.com/climatescape" className="underline hover:no-underline">Twitter</a>
       </footer>
     </div>
   )
