b283b5aece1479f9ab9998285dc4c0352cb8afe8
Jason Laster <jlaster@mozilla.com>
2020-02-11 14:36:08 -0700

    Add algolia search backend

---
+++ src/utils/algolia.js
@@ -0,0 +1,41 @@
+const { makeSlug } = require("./slug")
+
+const pageQuery = `query PagesQuery {
+  organizations: allAirtable(filter: {table: {eq: "Organizations"}}) {
+    nodes {
+      data {
+        Name
+        Logo {
+          raw {
+            url
+          }
+        }
+        Sector {
+          data {
+            Slug
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+const settings = { attributesToSnippet: [`excerpt:20`] }
+
+module.exports.queries = [
+  {
+    query: pageQuery,
+    transformer: ({ data }) => {
+      return data.organizations.nodes.map(({ data }) => ({
+        objectID: makeSlug(data.Name),
+        name: data.Name,
+        path: makeSlug(data.Name),
+        logo: !data.Logo ? "" : data.Logo.raw[0].url,
+        sector: !data.Sector ? "" : data.Sector.map(sector => sector.data.Slug),
+      }))
+    },
+    indexName: `Pages`,
+    settings,
+  },
+]
