8f9e9167449eca3e9f42e46263e6786be9efbc6c
Brendan Loudermilk <bloudermilk@gmail.com>
2020-02-24 17:08:57 +0100

    Revert "Add backend, part of #40"
    This reverts commit 7d198d061018bdc69b25b19c62a6e56c3093085d.

--- backend/postgres-init.sql
+++
@@ -1,2 +0,0 @@
--- Needed as per https://github.com/timgit/pg-boss/blob/master/docs/usage.md#database-installation
-CREATE EXTENSION pgcrypto;
--- backend/src/web.js
+++
@@ -1,9 +0,0 @@
-const buildFastify = require('./app');
-
-(async () => {
-    try {
-        await buildFastify();
-    } catch (e) {
-        console.error('Error starting fastify server', e);
-    }
-})();
