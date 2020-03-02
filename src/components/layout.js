4bb4a2bf7c7c7a17b6ec96db7f6db2b5a49f1534
Brendan Loudermilk <bloudermilk@gmail.com>
2020-03-02 11:59:02 +0100

    Add Netlify logo

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,6 +12,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import Navbar from "./Navbar"
 
 import "./layout.css"
+import NetlifyLogo from "../images/netlify.svg"
 
 const Layout = ({ children, contentClassName }) => {
   const data = useStaticQuery(graphql`
@@ -28,27 +29,34 @@ const Layout = ({ children, contentClassName }) => {
     <div className="flex flex-col min-h-screen antialiased">
       <Navbar />
       <main className={`flex-grow ${contentClassName}`}>{children}</main>
-      <footer className="text-gray-800 p-3 bg-gray-200 text-center sm:text-left">
-        <span>
-          ©{data.site.siteMetadata.title}
+      <footer className="text-gray-800 bg-gray-200 text-center sm:text-left py-3 sm:px-6 sm:flex items-center justify-between">
+        <div className="">
+          <span>
+            ©{data.site.siteMetadata.title}&nbsp;
+            <a
+              href="https://creativecommons.org/licenses/by-sa/4.0/"
+              className="underline hover:no-underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              CC BY-SA
+            </a>
+          </span>
+          <span> • </span>
           <a
-            href="https://creativecommons.org/licenses/by-sa/4.0/"
+            href="https://twitter.com/climatescape"
             className="underline hover:no-underline"
             target="_blank"
             rel="noopener noreferrer"
           >
-            CC BY-SA
+            Twitter
           </a>
-        </span>
-        <span> • </span>
-        <a
-          href="https://twitter.com/climatescape"
-          className="underline hover:no-underline"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Twitter
-        </a>
+        </div>
+        <div className="pt-3 sm:pt-0">
+          <a href="https://www.netlify.com" className="inline-block">
+            <img src={NetlifyLogo} alt="Deploys by Netlify" />
+          </a>
+        </div>
       </footer>
     </div>
   )
