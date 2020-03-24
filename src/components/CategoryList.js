0e8c12a4926e154d4c7574dd92239238eea6c5ec
Brendan Loudermilk <bloudermilk@gmail.com>
2020-03-24 11:45:15 +0100

    Clean up CategoryList component

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,23 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
+import classnames from "classnames"
 
 import "./CategoryList.css"
 
 function TopCategory({ category, pageContext }) {
-  const isSelected = category.id === pageContext.categoryId
+  const selected = category.id === pageContext.categoryId
 
   return (
-    <li
-      className={`category ${
-        isSelected ? "selected " : "underline"
-      } font-sans   text-gray-700`}
-    >
-      <Link
-        to={category.slug}
-        className={`${isSelected ? "bg-gray-300 " : ""}`}
-      >
-        {category.name}
-      </Link>
+    <li className={classnames("TopCategory ", { selected })}>
+      <Link to={category.slug}>{category.name}</Link>
     </li>
   )
 }
@@ -26,9 +18,9 @@ export default function({ categories, pageContext }) {
   const topLevelCategories = categories.filter(category => !category.parent)
 
   return (
-    <div className="left-sidebar hidden w-1/5 pl-6 lg:block">
-      <h3 className="mt-8 uppercase leading-9 text-gray-700">Categories</h3>
-      <ul className="category-list">
+    <div className="CategoryList leading-9 hidden w-1/5 pl-6 lg:block">
+      <h3 className="mt-8 uppercase text-gray-700">Categories</h3>
+      <ul>
         {topLevelCategories.map(category => (
           <TopCategory
             key={category.id}
