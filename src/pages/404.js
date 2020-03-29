931a185d5ccddcaf6187c76aa6c6b442e21fc969
Ron Braha <Ron.Braha@icloud.com>
2020-03-29 22:54:15 +0300

    404 text content

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,15 +7,19 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <div className="flex flex-col h-30">
-      <h1 className="text-center font-bold text-3xl m-2">NOT FOUND</h1>
-      <p className="text-center text-md h-12 lg:text-xl">
-        You just hit a route that doesn&#39;t exist... the sadness.
+    <div className="flex wrap-all flex-col h-30 mx-8">
+      <h1 className="text-center font-bold text-3xl m-2">PAGE NOT FOUND</h1>
+      <p className="text-center text-md h-20 md:h-12 lg:text-xl lg:h-12">
+        It looks like the page you&apos;re looking for doesn&apos;t exist.
+      </p>
+      <p className="text-center text-md h-20 md:h-12 lg:text-lg lg:h-12">
+        Try using the search bar at the top of the page or browse the website
+        from the homepage.
       </p>
       <p className="text-center m-2">
-        Return to{" "}
+        Visit the{" "}
         <Link to="/" className="text-blue-400">
-          Home Page
+          homepage
         </Link>
       </p>
     </div>
