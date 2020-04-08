4f1fdad202796bd713bf6ab4a2636b2e37afeb9f
Keola Morse <keolamorse@gmail.com>
2020-04-08 10:37:26 -0700

    updated className

--- src/templates/organization.js
+++ src/templates/organization.js
@@ -144,8 +144,6 @@ export default function OrganizationTemplate({ data }) {
   }))
 
   const img = getLogoImage(org)
-  // create variable to hide logo container if org doesn't have a logo
-  const showImg = !img ? "none" : "inline-block"
 
   return (
     <Layout contentClassName="bg-gray-100 font-sans">
@@ -155,10 +153,7 @@ export default function OrganizationTemplate({ data }) {
         <div className="mb-10 md:mt-6 flex flex-col lg:flex-row">
           <div className="w-5/5 lg:w-3/5">
             <div className="flex">
-              <div
-                className="mr-5 w-16 flex-shrink-0 block"
-                style={{ display: showImg }}
-              >
+              <div className={`${ !img ? `hidden`: `block` } mr-5 w-16 flex-shrink-0`}>
                 {img && (
                   <Img
                     fixed={img}
