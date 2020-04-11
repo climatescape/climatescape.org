80221ca36ffe0230ada659450c073d1e50d50a55
Loic <l.lefloch.91@gmail.com>
2020-04-11 13:07:35 +0200

    avoid pinterest interaction on homepage

--- src/components/TopicCard.js
+++ src/components/TopicCard.js
@@ -10,7 +10,10 @@ export default function TopicCard({ category }) {
         className="block rounded overflow-hidden shadow-md relative"
       >
         {category.cover && (
-          <Img fluid={category.cover} className="w-full h-40 object-cover" />
+          <Img
+            fluid={category.cover}
+            className="w-full h-40 object-cover pointer-events-none select-none"
+          />
         )}
         <div className="px-3 py-2 img-title-overlay w-full text-xl font-bold absolute bottom-0 left-0 text-white tracking-wide">
           {category.name}
