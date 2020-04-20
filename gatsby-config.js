6e1c74aec246990eab57d20851435334be049627
Loic <l.lefloch.91@gmail.com>
2020-04-20 18:27:47 +0200

    use .env AIRTABLE_BASE_ID property
    since we introduced the AIRTABLE_BASE_ID property on our .env files, this commit refactor our code to use it everywhere instead of having it hard-coded.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,16 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+// since AIRTABLE_BASE_ID was recently introduced, we add an error message to ask to update the
+// project configuration.
+if (!process.env.AIRTABLE_BASE_ID) {
+  console.log(
+    `AIRTABLE_BASE_ID property is missing from your env (.env.development file). 
+    See .env.sample as example.`
+  )
+  return
+}
+
 const config = {
   siteMetadata: {
     title: `Climatescape`,
@@ -30,7 +40,7 @@ const config = {
         apiKey: process.env.AIRTABLE_API_KEY,
         tables: [
           {
-            baseId: `appNYMWxGF1jMaf5V`,
+            baseId: process.env.AIRTABLE_BASE_ID,
             tableName: `Organizations`,
             tableView: `Published`,
             mapping: {
@@ -47,42 +57,42 @@ const config = {
             ],
           },
           {
-            baseId: `appNYMWxGF1jMaf5V`,
+            baseId: process.env.AIRTABLE_BASE_ID,
             tableName: "Contributors",
             tableView: "Published",
             mapping: { Avatar: `fileNode` },
           },
           {
-            baseId: `appNYMWxGF1jMaf5V`,
+            baseId: process.env.AIRTABLE_BASE_ID,
             tableName: "LinkedIn Profiles",
             tableView: "Grid view",
             mapping: { Logo: `fileNode` },
           },
           {
-            baseId: `appNYMWxGF1jMaf5V`,
+            baseId: process.env.AIRTABLE_BASE_ID,
             tableName: "Categories",
             mapping: { Cover: `fileNode` },
             tableView: "All Categories",
             tableLinks: [`Parent`, `Organizations`],
           },
           {
-            baseId: `appNYMWxGF1jMaf5V`,
+            baseId: process.env.AIRTABLE_BASE_ID,
             tableName: "Capital Profiles",
             tableLinks: [`Organization`, `Capital_Type`],
           },
           {
-            baseId: `appNYMWxGF1jMaf5V`,
+            baseId: process.env.AIRTABLE_BASE_ID,
             tableName: "Crunchbase ODM",
             tableLinks: [`Organization`],
             mapping: { Logo: `fileNode` },
           },
           {
-            baseId: `appNYMWxGF1jMaf5V`,
+            baseId: process.env.AIRTABLE_BASE_ID,
             tableName: "Sources",
             tableLinks: [`Organizations`],
           },
           {
-            baseId: `appNYMWxGF1jMaf5V`,
+            baseId: process.env.AIRTABLE_BASE_ID,
             tableName: "Capital Types",
             tableLinks: [`Capital_Profiles`],
             mapping: { Cover: `fileNode` },
