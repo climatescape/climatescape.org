332311bde858280674e8165027dd88bbfeffa4bc
Miriam Budayr <35577323+miriambudayr@users.noreply.github.com>
2020-05-06 15:46:30 -0500

    Lint

--- site/src/components/layout.js
+++ site/src/components/layout.js
@@ -7,7 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql, Link } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Navbar from "./Navbar"
 
