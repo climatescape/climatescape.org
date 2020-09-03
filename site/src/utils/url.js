33776f53bd3604c894a2afbbad54edabfff4a391
Ron Braha <Ron.Braha@icloud.com>
2020-09-03 13:08:46 +0300

    better lint

--- site/src/utils/url.js
+++ site/src/utils/url.js
@@ -3,14 +3,10 @@ const TwitterRegexp = /twitter\.com\/@?(\w+)/i
 export function parseTwitterHandle(url) {
   if (!url) return null
 
-  let handle
-
   try {
-    const [match] = url.match(TwitterRegexp)[1]
-    handle = match
+    const [handle] = url.match(TwitterRegexp)[1]
+    return handle && `@${handle}`
   } catch (e) {
     return null
   }
-
-  return handle && `@${handle}`
 }
